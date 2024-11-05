24820b9c010741eb9cc610575be5e94f9e72d70b
ldci4 <anoletenu@gmail.com>
2024-11-05 17:46:56 +0700

    lastfm

--- commands/fun/__init__.py
+++ commands/fun/__init__.py
@@ -1,12 +1,15 @@
 from typing import List
 
 available_commands: List[str] = [
-    'wordbomb'
+    'wordbomb',
+    'nowplaying',
+    'setlastfm'
 ]
 
 from .wordbomb import WordBomb
-
+from .lastfm import LastFM
 __all__ = [
     'WordBomb',
+    'LastFM',
     'available_commands'
 ]
